6c4ae3e4facaef377f8e5c1374d900a2b45bd609
thjnkme <thjnkme@gmail.com>
2015-03-31 13:16:44 +0700

    LoiNV7-Comit

--- www/js/highlight.js
+++ www/js/highlight.js
@@ -3,7 +3,6 @@ var currentHighlightEvent;
 var listImages;
 module.controller('HLController', function ($scope, $state)
 {
-    getHighlightEvents($scope);
     $scope.title = 'Sự kiện nổi bật';
     $scope.url = [
         {
@@ -25,10 +24,29 @@ module.controller('HLController', function ($scope, $state)
                 image: 'ion-camera',
                 video: 'ion-videocamera'
             };
+    $scope.statusLoadmore = true;
+    var curIndexLoaded = 0;
+    var init = false;
+    $scope.loadMore = function ()
+    {
+        var curLengthEvents = 0;
+        if (listHighlightEvent)
+            curLengthEvents = listHighlightEvent.length;
+        getHighlightEvents($scope, curIndexLoaded, curIndexLoaded + 5);
+        curIndexLoaded += 5;
+        if (curLengthEvents < listHighlightEvent.length)
+            $scope.statusLoadmore = true;
+        else
+            $scope.statusLoadmore = false;
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+        $scope.$broadcast('scroll.resize');
+    }
     $scope.doRefresh = function ()
     {
+        curIndexLoaded = 0;
         listHighlightEvent = null;
-        getHighlightEvents($scope);
+        $scope.loadMore();
+        $scope.$broadcast('scroll.refreshComplete');
     };
     $scope.pushPage = function (id)
     {
@@ -59,35 +77,38 @@ var indexOfUrl;
 
 module.controller('HLListViewController', function ($scope, $state)
 {
-    $scope.statusLoadmore=true;
-    var loadIndex = 5;
+    var loadedImage = false;
+    $scope.statusLoadmore = true;
+    var curIndexLoaded = 2;
     $scope.title = currentHighlightEvent.title;
-    getListImageItem($scope);
-    
+
     $scope.loadMoreImages = function ()
     {
-//        alert('begin load more');
-        if (listImages.rows[loadIndex + 1])
+        if (!loadedImage)
         {
-            $scope.rows.push(listImages.rows[++loadIndex]);
-            $scope.statusLoadmore=true;
+            getListImageItem($scope);
+            loadedImage = true;
         }
-        else $scope.statusLoadmore=false;
+        if (currentHighlightEvent.rows[curIndexLoaded])
+        {
+            $scope.rows.push(currentHighlightEvent.rows[curIndexLoaded++]);
+            $scope.statusLoadmore = true;
+        }
+        else
+            $scope.statusLoadmore = false;
         $scope.$broadcast('scroll.infiniteScrollComplete');
         $scope.$broadcast('scroll.resize');
-    }
+    };
     $scope.doRefresh = function ()
     {
-        getListImageItem($scope);
-        $scope.statusLoadmore=true;
-        loadIndex=5;
+        loadedImage = false;
+        curIndexLoaded = 2;
+        $scope.loadMoreImages();
+        $scope.$broadcast('scroll.refreshComplete');
     };
     $scope.showDetail = function (index, colIndex)
     {
-//        alert(colIndex);
-//        alert(index+colIndex);
         indexOfUrl = index * 2 + colIndex;
-
         $state.go('highlight_image', {title: ''});
     };
 });
@@ -95,7 +116,7 @@ module.controller('HLImageDetailController', function ($scope)
 {
     $scope.title = currentHighlightEvent.title;
     $scope.indexOfImage = indexOfUrl;
-    $scope.items = listImages.link;
+    $scope.items = currentHighlightEvent.link;
     $scope.width = window.innerWidth;
     $scope.height = window.innerHeight;
 });
@@ -113,138 +134,54 @@ module.controller('HLVideoController', function ($scope, $location, $state)
 //Get list image item by curent index highlight
 function getListImageItem($scope)
 {
-        $scope.rows = [];
-        listImages = {};
-        listImages.rows = [];
-        listImages.link = [
-            {
-                "name": "tfss-0a1099cc-2a23-4fd2-a0da-d489c22189ca-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-0a1099cc-2a23-4fd2-a0da-d489c22189ca-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-e23e9388-646f-492f-b98d-7c75f08f8973-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-e23e9388-646f-492f-b98d-7c75f08f8973-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-8570d638-1870-419e-898a-275d0dbdb3a8-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-8570d638-1870-419e-898a-275d0dbdb3a8-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-95535d66-155d-408e-b15e-16ec155fc899-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-95535d66-155d-408e-b15e-16ec155fc899-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-f6ca316f-0f09-421e-9366-533728da7b75-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-f6ca316f-0f09-421e-9366-533728da7b75-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-0166cb10-7faf-4b47-b4a3-1b0bd9995443-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-0166cb10-7faf-4b47-b4a3-1b0bd9995443-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-b657051b-89e2-44c6-8642-d3e33253ce49-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-b657051b-89e2-44c6-8642-d3e33253ce49-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-d9544353-4235-468a-9cfd-10dc5e6e8431-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-d9544353-4235-468a-9cfd-10dc5e6e8431-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-87bc46f6-90d1-417a-afc8-1cf80d0250d0-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-87bc46f6-90d1-417a-afc8-1cf80d0250d0-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-b81ade70-4ccf-4529-be15-d6975e88d279-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-b81ade70-4ccf-4529-be15-d6975e88d279-feedback_image.jpg"
-            },
-            {
-                "__type": "File",
-                "name": "tfss-4c149795-8335-4d50-9b0f-679b17f3f2bc-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-4c149795-8335-4d50-9b0f-679b17f3f2bc-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-3bf3386a-812c-4941-a0c3-840c11fa7bb0-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-3bf3386a-812c-4941-a0c3-840c11fa7bb0-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-41c6fd94-0f2b-482c-8152-8d0a6c5a19df-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-41c6fd94-0f2b-482c-8152-8d0a6c5a19df-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-62b8aef5-4f8d-4586-864b-2d20346e81c8-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-62b8aef5-4f8d-4586-864b-2d20346e81c8-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-bb225523-802c-4508-97ec-5329993ed222-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-bb225523-802c-4508-97ec-5329993ed222-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-2375eefd-fde4-4b3f-95be-3014407ff3da-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-2375eefd-fde4-4b3f-95be-3014407ff3da-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-bef18e63-4a5b-4333-8027-a4f320ff6ddb-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-bef18e63-4a5b-4333-8027-a4f320ff6ddb-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-3173c0dd-6d24-4e47-9afe-c652ea8639a5-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-3173c0dd-6d24-4e47-9afe-c652ea8639a5-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-a0ededf7-9f56-48b3-97a1-3a3d837f9572-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-a0ededf7-9f56-48b3-97a1-3a3d837f9572-feedback_image.jpg"
-            },
-            {
-                "name": "tfss-1aba0b0b-9ae8-4e4d-af45-de4554091d68-feedback_image.jpg",
-                "url": "http://files.parsetfss.com/15723333-8130-4869-8e9e-c359c9a3a944/tfss-1aba0b0b-9ae8-4e4d-af45-de4554091d68-feedback_image.jpg"
-            }
-        ];
-        var j=0;
-        for (var i = 0; i < listImages.link.length; i++)
+    $scope.rows = [];
+    currentHighlightEvent.rows = [];
+    var j = 0;
+    for (var i = 0; i < currentHighlightEvent.link.length; i++)
+    {
+        currentHighlightEvent.rows.push(
+                {
+                    cells:
+                            [
+                                {
+                                    url: currentHighlightEvent.link[i].url
+                                },
+                                {
+                                    url: currentHighlightEvent.link[++i].url
+                                }
+                            ]
+                });
+        if (j < 2 && currentHighlightEvent.rows[j])
         {
-            listImages.rows = listImages.rows.concat(
-                    [{
-                            cells:
-                                    [
-                                        {
-                                            url: listImages.link[i].url
-                                        },
-                                        {
-                                            url: listImages.link[++i].url
-                                        }
-                                    ]
-                        }]);
-            if (j < 6)
-            {
-                $scope.rows.push(listImages.rows[j++]);
-            }
-                
+            $scope.rows.push(currentHighlightEvent.rows[j++]);
         }
-        $scope.$broadcast('scroll.refreshComplete');
+    }
 }
 
-function getHighlightEvents($scope)
+function getHighlightEvents($scope, begin, end)
 {
     if (!listHighlightEvent)
     {
         listHighlightEvent = {};
-        $.post(PARSE + "onLoadHighlightEvent", {userId: userId, session: session, begin: 0, end: 50})
-                .done(function (data)
+        listHighlightEvent.items = [];
+        $scope.items = [];
+    }
+    $.post(PARSE + "onLoadHighlightEvent", {userId: userId, session: session, begin: begin, end: end})
+            .done(function (data)
+            {
+                if (data.result.length > 0)
                 {
                     $scope.$apply(function ()
                     {
-                        $scope.$broadcast('scroll.refreshComplete');
-                        listHighlightEvent.items = data.result;
-                        $scope.items = listHighlightEvent.items;
+                        listHighlightEvent.items = listHighlightEvent.items.concat(data.result);
+                        $scope.items = $scope.items.concat(data.result);
                     });
-                }).fail(function (err)
-        {
-            listHighlightEvent.items = {};
-            alert('Xin hãy kiểm tra lại kết nối');
-        });
-    }
-    else
-        $scope.items = listHighlightEvent.items;
+                }
+            }).fail(function (err)
+    {
+        alert('Xin hãy kiểm tra lại kết nối');
+    });
+    $scope.items = listHighlightEvent.items;
 }
 function getHighlightObjectById(id)
 {
