8fe6dff7b9824cb4eb9481f825bf76cea97b8f4c
thjnkme <thjnkme@gmail.com>
2015-04-03 06:18:58 +0700

    COMIT LOINV

--- plugins_custom/vt.plugins.textview/src/ios/VtTextView.m
+++ plugins_custom/vt.plugins.textview/src/ios/VtTextView.m
@@ -17,17 +17,83 @@
     textView.layer.cornerRadius = 5;
     textView.center=CGPointMake(self.webView.frame.size.width/2, y);
     textView.backgroundColor=[UIColor whiteColor];
-    [self.viewController.view addSubview:textView];
-    
-//    UITextField *textField=[[UITextField alloc] initWithFrame:CGRectMake(0, y-100, width, 44)];
-//    textField.center=CGPointMake(self.webView.frame.size.width/2, y-100);
-//    [textField setBackgroundColor:[UIColor greenColor]];
-//    [self.viewController.view addSubview:textField];
-    
+    textView.delegate=self;
+    [self.webView addSubview:textView];
+    UIPanGestureRecognizer *panGesture=[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)];
+    panGesture.delegate=self;
+    [self.viewController.view addGestureRecognizer:panGesture];
+
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:@"OK"];
+    
     [commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+}
+
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+//    CGRect frame=textView.frame;
+//    frame.origin.y=[UIScreen mainScreen].bounds.size.height-kbSize.height-frame.size.height;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        CGRect frame=textView.frame;
+        frame.origin.y=[UIScreen mainScreen].bounds.size.height-kbSize.height-frame.size.height;
+        [textView setFrame:frame];
+    }];
+}
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+//    NSDictionary* info = [aNotification userInfo];
+//    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+//    
+//    //    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+//    [UIView animateWithDuration:1.0 animations:^{
+//        CGRect frame=textView.frame;
+//        //        CGRect rect=[UIScreen mainScreen].bounds.size.width;
+//        frame.origin.y=[UIScreen mainScreen].bounds.size.height-kbSize.height;
+//        [textView setFrame:frame];
+//    }];
+}
+- (void)textViewDidBeginEditing:(UITextView *)textView
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+}
+- (void) textViewDidEndEditing:(UITextView *)textView
+{
+    [textView resignFirstResponder];
+}
+//To enable gesture recognizer
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
+}
+-(void) panGesture:(UIPanGestureRecognizer*) panGesture
+{
+    CGRect frame=textView.frame;
+    
+    if(panGesture.state==UIGestureRecognizerStateBegan)
+    {
+    }
+    if (panGesture.state==UIGestureRecognizerStateChanged)
+    {
+        float pos=[[self.webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('fbNoiDung').getBoundingClientRect().bottom+5"] intValue];
+        frame.origin.y=pos;
+        [textView setFrame:frame];
+//        textView.text=[NSString stringWithFormat:@"%f",self.webView.contentScaleFactor];
+    }
+    if(panGesture.state==UIGestureRecognizerStateEnded)
+    {
+        [self.webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('FeedBackContent').getAttribute('direction')=z"];
+    }
 }
 -(void) setFrame:(CDVInvokedUrlCommand*)command
 {
@@ -49,7 +115,7 @@
     
     [commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
--(void) getText:(CDVInvokedUrlCommand*)command
+-(void) getValueTextView:(CDVInvokedUrlCommand*)command
 {
     NSString* callbackId = [command callbackId];
     NSString* content = textView.text;
@@ -59,7 +125,7 @@
     
     [commandDelegate sendPluginResult:result callbackId:callbackId];
 }
--(void) hide:(CDVInvokedUrlCommand*)command
+-(void) hidden:(CDVInvokedUrlCommand*)command
 {
     textView.hidden=YES;
     
@@ -85,7 +151,7 @@
                                messageAsString:@"OK"];
     [commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
--(void) setText:(CDVInvokedUrlCommand*)command
+-(void) clearText:(CDVInvokedUrlCommand*)command
 {
     textView.text=@"";
     CDVPluginResult* result = [CDVPluginResult
