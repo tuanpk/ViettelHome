8dd70dbf08db0bcb1dd01b5ba191e49aed91b9a6
thjnkme <thjnkme@gmail.com>
2015-04-03 08:50:18 +0700

    LoiNV7-Comit

--- plugins_custom/vt.plugins.textview/src/ios/VtTextView.m
+++ plugins_custom/vt.plugins.textview/src/ios/VtTextView.m
@@ -31,11 +31,11 @@
 
 -(void) initWithID : (CDVInvokedUrlCommand*) command {
     NSDictionary *dictFrame = [command argumentAtIndex : 0];
-    float pos = [[self.webView stringByEvaluatingJavaScriptFromString :[NSString stringWithFormat:@"document.getElementById('%@').getBoundingClientRect().bottom+5",] [dictFrame objectForKey : @"id"]] intValue];
-    frame.origin.y = pos;
-    float width = [[dictFrame objectForKey : @"width"] floatValue];
-    float height = [[dictFrame objectForKey : @"height"] floatValue];
-    textView = [[UITextView alloc] initWithFrame : CGRectMake(0, pos, width, height)];
+    float pos = [[self.webView stringByEvaluatingJavaScriptFromString :[NSString stringWithFormat:@"document.getElementById('%@').getBoundingClientRect().bottom+5",[dictFrame objectForKey : @"id"]]] intValue];
+    float perwidth = [[dictFrame objectForKey : @"width"] floatValue];
+    float perheight = [[dictFrame objectForKey : @"height"] floatValue];
+    CGSize screenSize= [UIScreen mainScreen].bounds.size;
+    textView = [[UITextView alloc] initWithFrame : CGRectMake(0, pos, perwidth*screenSize.width/100, perheight*screenSize.height/100)];
     textView.center = CGPointMake(self.webView.frame.size.width / 2, pos);
     
     [textView.layer setBorderColor : [[[UIColor blackColor] colorWithAlphaComponent : 0.8] CGColor]];
@@ -62,10 +62,11 @@
     CGSize kbSize = [[info objectForKey : UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
 
     [self.webView.scrollView setContentOffset:CGPointMake(0, textView.frame.size.height)];
-    [self.webView.scrollView setContentSize:CGSizeMake(self.webView.frame.size.width, self.webView.frame.size.height+textView.frame.size.height)];
+    [self.webView.scrollView setContentSize:CGSizeMake(self.webView.frame.size.width, self.webView.frame.size.height+textView.frame.size.height*2)];
     float pos = [[self.webView stringByEvaluatingJavaScriptFromString :@"document.getElementById('fbNoiDung').getBoundingClientRect().bottom+5"] floatValue];
+     CGRect frame = textView.frame;
     frame.origin.y = pos;
-    textView.frame=frame;
+    textView.frame= frame;
 
 //    [UIView animateWithDuration : 0.2 animations : ^{
 //        CGRect frame = textView.frame;
